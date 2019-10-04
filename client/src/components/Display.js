cb3660bd35e0d1015ddefc6208122fa69d445e83
GUL-Bao <BaoPham92@yahoo.com>
2019-10-04 11:59:06 -0400

    List item 2 done && CSS styles

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // * COMPONENT IMPORTS HERE
 import { Players } from './Players';
 
 export default class Display extends Component {
 
-    render() {
+    // * STATE
+    state = {
+        roster: []
+    }
 
+    // * LOGIC FOR WHEN COMPONENT MOUNTS
+    componentWillMount() {
+        // ? FETCH AND SET PLAYER DATA
+        axios.get('http://localhost:5000/api/players')
+            .then(res => this.setState({ roster: [...res.data] }))
+    }
+
+    render() {
         return (
             <div>
                 <h1>DISPLAY COMPONENT</h1>
-                <Players />
+
+                {/* // * IF ROSTER EXIST, RENDER PLAYERS WITH ROSTER
+                */}
+
+                {
+                    this.state.roster && <Players roster={this.state.roster} />
+                }
             </div>
         )
     }
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-export const Players = () => {
+export const Players = ({ roster }) => {
+
+    // * MAKE SURE WE HAVE DATA
+    if (!!roster === false) {
+        return <span>Loading roster of players</span>
+    }
 
     return (
         <div>
             <h2>PLAYERS COMPONENT</h2>
+            <ul>
+            {/* // * IF ROSTER EXIST, RENDER LIST ELEMENTS WITH PLAYER NAMES 
+            */}
+                {
+                    roster && roster.map((player, key) => {
+                        return <li key={key}>{player.name}</li>
+                    })
+                }
+            </ul>
         </div>
     )
 }
